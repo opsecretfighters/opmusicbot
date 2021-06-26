86e7dc64705f0c01bba05e80081e80e66516f913
amaninamdar09 <79567211+amaninamdar09@users.noreply.github.com>
2021-06-26 16:07:36 +0530

    Update Ytplay.py

--- handlers/Ytplay.py
+++ handlers/Ytplay.py
@@ -57,20 +57,8 @@ async def play(_, message: Message):
         print(str(e))
         return
 
-    def r_ply(type_):
-    if type_ == 'play':
-        ico = '▶'
-    else:
-        ico = '⏸'
-    mar = InlineKeyboardMarkup(
-        [
+    keyboard = InlineKeyboardMarkup(
             [
-                InlineKeyboardButton('⏹', 'leave'),
-                InlineKeyboardButton('⏸', 'puse'),
-                InlineKeyboardButton('▶️', 'resume'),
-                InlineKeyboardButton('⏭', 'skip')
-                
-            ],
                 [
                     InlineKeyboardButton(
                         text="Watch On YouTube 🎬",
